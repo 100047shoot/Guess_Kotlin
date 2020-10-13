add7ed2c8cd004a70e7d2bfe1cd34574cb90220e
qscvb952 <qscvb952@gmail.com>
2020-10-13 11:16:54 +0800

    initial commit

---
+++ app/src/main/java/com/d/guessnum/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.d.guessnum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.id.hello_main
+        R.drawable.pig
+        R.string.ok
+        R.color.messageColor
+
+        //123
+        /*123*/
+        //TODO
+
+    }
+}
